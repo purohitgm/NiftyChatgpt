6e905b2c0a7db4033ed2495d3fb0339b9321166b
purohitgm <gururaj.purohit@gmail.com>
2026-03-09 23:20:01 +0530

    Add files via upload

--- Core/indicators.py
+++ Core/indicators.py
@@ -1,1 +1,9 @@
+import ta
 
+def indicators(df):
+    df["RSI"] = ta.momentum.RSIIndicator(df["Close"],14).rsi()
+    df["EMA20"] = df["Close"].ewm(span=20).mean()
+    df["EMA50"] = df["Close"].ewm(span=50).mean()
+    df["EMA200"] = df["Close"].ewm(span=200).mean()
+    df["VOL20"] = df["Volume"].rolling(20).mean()
+    return df
